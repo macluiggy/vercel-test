2d81631d43f57be99812bcf49226005a1ad48e12
macluiggy <ferrinluiggy@gmail.com>
2021-10-12 15:36:04 -0500

    jejejej

--- api/app.js
+++ api/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express()
 
+const path = require('path');
+
+//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+//app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+	console.log(__dirname + '/public/index.html')
+	res.send('hola mi compa')
+})
+
+
 app.get('/hola', (req, res) => {
 	res.json({
 		hola: 'hello'
 	})
 })
+
 app.get('/hello', (req, res) => {
 	res.json({
 		hola: 'hellouuuu'
@@ -14,5 +26,6 @@ app.get('/hello', (req, res) => {
 
 const port = process.env.PORT || 3000
 const listen = app.listen(port, () => console.log('App is listening in ' + port))
+module.exports = listen
 
 module.exports = app
--- index.js
+++ index.js
@@ -3,4 +3,3 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
