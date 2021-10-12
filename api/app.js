9a767c320dd9c1a9660f2a26c8992aa49670406f
macluiggy <ferrinluiggy@gmail.com>
2021-10-12 14:59:27 -0500

    jejeje

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/hola', (req, res) => {
 	res.json({
 		hola: 'hello'
 	})
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 require('./api/app.js')
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
